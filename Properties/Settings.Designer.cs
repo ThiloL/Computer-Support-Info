32b0d8e56e090d73e53743247f970e69b85a326f
Thilo <login@thilo-langbein.de>
2023-09-01 20:18:53 +0200

    s

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -43,8 +43,8 @@ namespace Computer_Support_Info.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ArrayOfString xmlns:xsi=\"http://www.w3." +
-            "org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">\r\n  <s" +
+        [global::System.Configuration.DefaultSettingValueAttribute("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ArrayOfString xmlns:xsd=\"http://www.w3." +
+            "org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">\r\n  <s" +
             "tring>Microsoft Defender Antivirus</string>\r\n</ArrayOfString>")]
         public global::System.Collections.Specialized.StringCollection update_categories_to_exclude {
             get {
